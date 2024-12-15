a808a8b2735f2b6ff54723c7573f9518d9323441
apakr <megapuppypower@gmail.com>
2024-12-15 14:58:07 -0600

    started working on monte carlo sim for maximizing sharpe ratio

--- monte_carlo_max_sharpe_sim.py
+++ monte_carlo_max_sharpe_sim.py
@@ -1,5 +1,10 @@
 import pandas as pd
-import analysis_script as og_script
+import analysis_functs as funct
 import numpy as np
 import yfinance as yf
+
+start_date = "2023-11-16" # hard coded dates for analysis
+end_date = "2024-11-16"
+
+portf = pd.read_csv("simple_portf.csv")
 
